70198da29c5beb020a3a59cb1766d7a0dba69237
Kseniya-Korolchuk <kkorolchuk@gmail.com>
2022-05-20 20:32:01 +0300

    feat: 5th task complete

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,9 +4,21 @@ const srcPath = path.join(__dirname, 'files');
 const srcDest = path.join(__dirname, 'files-copy');
 
 fs.mkdir(srcDest, {recursive: true}, function(){});
-fs.readdir(srcPath, function (err, items) {
-  for (let i = 0; i < items.length; i++) {
-    console.log(items[i]);
-    fs.copyFile(path.join(srcPath,items[i]), path.join(srcDest,items[i]), function(){});
+fs.stat(srcDest, function(err) {
+  if (!err) {
+    fs.readdir(srcDest, function (err, items) {
+      for (let i = 0; i < items.length; i++) {
+        fs.unlink(path.join(srcDest, items[i]), function(err){
+          if (err) console.log(err);
+          console.log(`файл ${items[i]} удален`);  
+        });
+      }
+    });
   }
+  fs.readdir(srcPath, function (err, items) {
+    for (let i = 0; i < items.length; i++) {
+      fs.copyFile(path.join(srcPath,items[i]), path.join(srcDest,items[i]), function(){});
+      console.log(`файл ${items[i]} скопирован`);    
+    }
+  });
 });
