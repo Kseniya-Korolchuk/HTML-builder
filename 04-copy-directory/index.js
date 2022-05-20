7ca1c64d154b1eee848084b7e728ef14fe314035
Kseniya-Korolchuk <kkorolchuk@gmail.com>
2022-05-20 16:14:02 +0300

    feat: 4th task complete

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const srcPath = path.join(__dirname, 'files');
+const srcDest = path.join(__dirname, 'files-copy');
 
+fs.mkdir(srcDest, {recursive: true}, function(){});
+fs.readdir(srcPath, function (err, items) {
+  for (let i = 0; i < items.length; i++) {
+    console.log(items[i]);
+    fs.copyFile(path.join(srcPath,items[i]), path.join(srcDest,items[i]), function(){});
+  }
+});
